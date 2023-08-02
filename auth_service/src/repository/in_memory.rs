4106aaf5425512cc1f8d6d9603892cc8c68ee8a7
Daniel Bigos <daniel.bigos@icloud.com>
2023-08-02 09:11:07 +0200

    test: add InMemoryRepository tests

--- auth_service/src/repository/in_memory.rs
+++ auth_service/src/repository/in_memory.rs
@@ -27,11 +27,11 @@ impl<T: Clone> InMemoryRepository<T> {
 
     fn do_insert(&mut self, key: &str, value: &T) -> DBResult<()> {
         match self.do_exists(key) {
-            true => match self.do_put(key, value) {
+            false => match self.do_put(key, value) {
                 None => Ok(()),
                 Some(_) => panic!("User for key {:?} should not exist", key),
             },
-            false => Err(RepositoryError::ValueAlreadyExists),
+            true => Err(RepositoryError::ValueAlreadyExists),
         }
     }
 
@@ -45,5 +45,70 @@ impl<T: Clone> InMemoryRepository<T> {
 
     fn do_exists(&self, key: &str) -> bool {
         self.items.contains_key(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const DB_KEY: &str = "test_key";
+
+    fn init() -> InMemoryRepository<String> {
+        InMemoryRepository::<String>::default()
+    }
+
+    #[test]
+    fn should_not_exist() {
+        let db = init();
+        assert!(!db.exists(DB_KEY).unwrap());
+        assert_eq!(db.get(DB_KEY).unwrap(), None);
+    }
+
+    #[test]
+    fn should_insert() {
+        let mut db = init();
+        let value: String = String::from("test value");
+
+        assert!(!db.exists(DB_KEY).unwrap());
+        assert_eq!(db.insert(DB_KEY, &value), Ok(()));
+        assert!(db.exists(DB_KEY).unwrap());
+        assert_eq!(db.get(DB_KEY).unwrap(), Some(value));
+    }
+
+    #[test]
+    fn should_not_insert_twice() {
+        let mut db = init();
+        let value: String = String::from("test value");
+
+        assert!(!db.exists(DB_KEY).unwrap());
+        assert_eq!(db.insert(DB_KEY, &value), Ok(()));
+        assert_eq!(
+            db.insert(DB_KEY, &value),
+            Err(RepositoryError::ValueAlreadyExists)
+        );
+    }
+
+    #[test]
+    fn should_put_nonexistent_value() {
+        let mut db = init();
+        let value: String = String::from("test value");
+
+        assert!(!db.exists(DB_KEY).unwrap());
+        assert_eq!(db.put(DB_KEY, &value), Ok(()));
+        assert!(db.exists(DB_KEY).unwrap());
+        assert_eq!(db.get(DB_KEY).unwrap(), Some(value));
+    }
+
+    #[test]
+    fn should_update_existent_value_with_put() {
+        let mut db = init();
+        let value: String = String::from("test value");
+        let updated_value: String = String::from("updated value");
+
+        assert!(!db.exists(DB_KEY).unwrap());
+        assert_eq!(db.insert(DB_KEY, &value), Ok(()));
+        assert_eq!(db.put(DB_KEY, &updated_value), Ok(()));
+        assert_eq!(db.get(DB_KEY).unwrap(), Some(updated_value));
     }
 }
